9b41ac5bc8a2c78bf783ec571648a7a50cae681f
PrincessBeta <Princessbranlobein@pm.me>
2023-04-20 16:52:18 +0200

    EX_2

--- src/ilu2/Welcome.java
+++ src/ilu2/Welcome.java
@@ -3,8 +3,13 @@ package ilu2;
 public class Welcome {
 	public static String welcome(String input) {
 		StringBuilder chaine = new StringBuilder();
+		
 		chaine.append("Hello, ");
-		chaine.append(input.substring(0,1).toUpperCase() + input.substring(1));
+		
+		if (input.trim()!="")
+			chaine.append(input.substring(0,1).toUpperCase() + input.substring(1));
+		else
+			chaine.append("my friend");
 		
 		return chaine.toString();
 	}
